a9d090306c841e86c582811f21399a08f55aed17
Ezekiel Smithburg <tehgeekmeister@gmail.com>
2013-04-09 00:40:35 -0700

    anything potentially confusing should be commented in this project, because it's for helping people learn, so even the source should do that.

--- language.js
+++ language.js
@@ -3,6 +3,8 @@ function tokenizer (input) {
   var buffer = ''
   var steps = []
 
+  // since we're not passing buffer in explicitly, it references the one a few lines above.
+  // this means we can clear buffer from inside of this function.  yay procedural style!
   function clear_buffer () {
     if (buffer != '') {
       output.push(buffer)
@@ -13,13 +15,15 @@ function tokenizer (input) {
   for (var i = 0; i < input.length; i++) {
     // this is a start.  would probably be better to both accumulate the steps, and have a callback.
     steps.push({
-      'output' : output.slice(0),
+      'output' : output.slice(0), // this is a trick to make a copy of output, rather than a reference to it.
       'buffer' : buffer,
     })
     switch(input[i]) {
       case '(':
         output.push('(')
         break
+        // if you are confused why this is here, it is because case
+        // statements automatically continue to the next case.
       case ')':
         clear_buffer(buffer)
         output.push(')')
